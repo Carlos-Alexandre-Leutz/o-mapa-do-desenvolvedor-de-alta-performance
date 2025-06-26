f5a48af6f8826ed20b75cb368723132f61f122e8
Carlos Alexandre Leutz <leutzeleutz@gmail.com>
2025-06-26 13:39:28 -0300

    hotfix: ajuste no botão de comprar

--- assets/js/modalPlay.js
+++ assets/js/modalPlay.js
@@ -18,15 +18,15 @@ videoElement.onplay = function() {
 function desmutar() {
     // videoElement.currentTime = 0;
     // videoElement.muted = false;
+    let btnCompraLink = document.getElementById('btnCompraLink');
+    btnCompraLink.style.display = 'block';
+    let btnCompra = document.getElementById('btnCompra');
+    btnCompra.style.display = 'none';
+    botaoDesmutarClicado();
     var iframe = document.getElementById("videoFrame");
     var videoSrc = "https://www.youtube.com/embed/5fHtpLExs2c?autoplay=1&controls=0&start=2";
 
     // Reinicia o vídeo e ativa o som removendo o mute
     iframe.src = videoSrc + "&mute=0";
     modalAlerta.style.display = 'none';
-    let btnCompraLink = document.getElementById('btnCompraLink');
-    btnCompraLink.style.display = 'block';
-    let btnCompra = document.getElementById('btnCompra');
-    btnCompra.style.display = 'none';
-    botaoDesmutarClicado();
 }
