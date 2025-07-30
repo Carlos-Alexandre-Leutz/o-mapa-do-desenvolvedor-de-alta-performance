5845b8b0cc2ca05638d783e128279f03e8b6be01
Carlos-Alexandre-Leutz <leutzeleutz@gmail.com>
2025-07-30 16:45:33 -0300

    feature: trocando vídeo de vendas

--- assets/js/modalPlay.js
+++ assets/js/modalPlay.js
@@ -24,7 +24,8 @@ function desmutar() {
     btnCompra.style.display = 'none';
     botaoDesmutarClicado();
     var iframe = document.getElementById("videoFrame");
-    var videoSrc = "https://www.youtube.com/embed/5fHtpLExs2c?autoplay=1&controls=0&start=2";
+    // var videoSrc = "https://www.youtube.com/embed/5fHtpLExs2c?autoplay=1&controls=0&start=2";
+    var videoSrc = "https://www.youtube.com/embed/rXEpCLIKbdY?autoplay=1&controls=0";
 
     // Reinicia o vídeo e ativa o som removendo o mute
     iframe.src = videoSrc + "&mute=0";
