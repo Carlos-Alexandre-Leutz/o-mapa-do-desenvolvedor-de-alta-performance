7615f5303f038cc65246ef5b18dcdb40c71ddde6
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2026-01-11 21:50:58 -0300

    feature:ajuste no video/

--- assets/js/modalPlay.js
+++ assets/js/modalPlay.js
@@ -28,7 +28,7 @@ function desmutar() {
     var iframe = document.getElementById("videoFrame");
     // var videoSrc = "https://www.youtube.com/embed/5fHtpLExs2c?autoplay=1&controls=0&start=2";
     // var videoSrc = "https://www.youtube.com/embed/rXEpCLIKbdY?autoplay=1&controls=0";
-    var videoSrc = "https://www.youtube.com/embed/fETwgEJL7VU?autoplay=1&controls=0";
+    var videoSrc = "https://www.youtube.com/embed/UCqrggoWA30?autoplay=1&controls=0";
 
     // Reinicia o vídeo e ativa o som removendo o mute
     iframe.src = videoSrc + "&mute=0";
